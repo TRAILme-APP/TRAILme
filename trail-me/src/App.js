f4738a1e6701928d68555db025fd240842c9d66b
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-09-22 16:49:03 -0500

    created profile component, imported to app.js, installed react-json-pretty package

--- trail-me/src/App.js
+++ trail-me/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
       <LoginButton />
       <LogoutButton />
+      <Profile />
     </>
   );
 }
