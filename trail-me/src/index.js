d5196255c43966108e4f92e42df30237e30c3f5e
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-09-22 12:27:51 -0500

    auth0 packaged installed and enviroment variables created

--- trail-me/src/index.js
+++ trail-me/src/index.js
@@ -4,6 +4,9 @@ import App from "./App";
 
 console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
