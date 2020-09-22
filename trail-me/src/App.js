ae5d8f88c94aba5acd603d103df1925f90e993bb
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-09-22 16:38:05 -0500

    created logout btn and imported to app.js

--- trail-me/src/App.js
+++ trail-me/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
-  return <LoginButton />;
+  return (
+    <>
+      <LoginButton />
+      <LogoutButton />
+    </>
+  );
 }
 
 export default App;
