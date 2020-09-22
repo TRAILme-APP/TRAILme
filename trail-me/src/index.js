7978692f85086331ae549b588fb90ab256f457f2
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-09-22 12:34:46 -0500

    imported Auth0 Provider and wrapped app

--- trail-me/src/index.js
+++ trail-me/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
 
@@ -8,8 +9,12 @@ const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById("root")
 );
