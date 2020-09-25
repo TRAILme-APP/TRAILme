87f74fe1b5c474849128805c2395de658f722d13
steveo9219 <stephen.guzman92@gmail.com>
2020-09-24 20:35:18 -0500

    added boilerplate backend to recieve a username and write to DB

--- trail-me/server.js
+++ trail-me/server.js
@@ -1,28 +1,33 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+const Username = require("../trail-me/models/Username");
 const app = express();
 
 app.use(logger("dev"));
 
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/trailme", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/trailme", {
+	useNewUrlParser: true,
 });
 
-// routes
-app.use(require("./routes/api.js"));
+app.post("/submit", ({ body }, res) => {
+	Username.create(body)
+		.then((dbUser) => {
+			res.json(dbUser);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+	console.log(`App running on port ${PORT}!`);
 });
