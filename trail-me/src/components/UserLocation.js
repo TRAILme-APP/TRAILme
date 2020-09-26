c8deb8576821c0d50f36abce7159329dd44a2689
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-09-25 20:19:37 -0500

    added icon img to dropdown & css

--- trail-me/src/components/UserLocation.js
+++ trail-me/src/components/UserLocation.js
@@ -46,7 +46,19 @@ class UserLocation extends Component {
       <div className="Location">
         <Dropdown className="LocationDropdown">
           <Dropdown.Toggle variant="success" id="dropdown-basic">
-            Cities
+            <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              className="bi bi-geo-alt-fill"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+              />
+            </svg>
           </Dropdown.Toggle>
 
           <Dropdown.Menu onClick={() => this.handleClick()}>
@@ -59,7 +71,6 @@ class UserLocation extends Component {
             <Dropdown.Item href="#">Lubbock</Dropdown.Item>
             <Dropdown.Item href="#">San Antonio</Dropdown.Item>
             <Dropdown.Item href="#">Waco</Dropdown.Item>
-            <Dropdown.Item href="#"> * Use My Location</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
 
