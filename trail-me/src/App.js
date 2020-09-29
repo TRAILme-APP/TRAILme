3d2fccaec4a7c26eda37fbca3736e6de71d2e5ce
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-09-28 19:29:30 -0500

    removed redudant userLocation and added moments to json

--- trail-me/src/App.js
+++ trail-me/src/App.js
@@ -2,17 +2,14 @@ import React from "react";
 import "./App.css";
 import Profile from "./components/Profile";
 import { useAuth0 } from "@auth0/auth0-react";
-import Navbar from "./components/Navbar"
-import OpenWeather from "./components/OpenWeather"
 import Navbar from "./components/Navbar";
+import OpenWeather from "./components/OpenWeather";
 
 import SelectPath from "./components/SelectPath";
-
 
 import UserLocation from "./components/UserLocation";
 
 require("dotenv").config();
-
 
 function App() {
   const { isLoading } = useAuth0();
@@ -26,10 +23,8 @@ function App() {
 
       <SelectPath />
 
-      <OpenWeather /> 
-     <UserLocation />
-
-
+      <OpenWeather />
+      <UserLocation />
     </div>
   );
 }
--- trail-me/src/components/SelectPath/index.js
+++ trail-me/src/components/SelectPath/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 import UserLocation from "../UserLocation";
 import Diffcultylevel from "../Diffcultylevel";
 import { Container, Row, Col, Button } from "react-bootstrap";
 
 export default function index() {
-    return (
-        <div className="selectPath">
-            <Container>
-                <Row>
-                    <Col sm={12} md={6}><UserLocation /></Col>
-                    <Col sm={12} md={6}><Diffcultylevel /></Col>
-                    <Col sm={12}><Button>Let's Go!</Button></Col>
-                </Row>
-            </Container>
-
-
-
-        </div>
-    )
+  return (
+    <div className="selectPath">
+      <Container>
+        <Row>
+          <Col sm={12} md={6}>
+            <Diffcultylevel />
+          </Col>
+          <Col sm={12}>
+            <Button>Let's Go!</Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
