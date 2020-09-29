2a83c0beac7f493d9b46c455e164adfe1b3d34c1
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-09-28 20:44:37 -0500

    removed app name from navbar

--- trail-me/src/components/Navbar/index.jsx
+++ trail-me/src/components/Navbar/index.jsx
@@ -15,7 +15,6 @@ import {
 export default function index() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="#home">TRAILme</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
